2d1b09ca3aa3e28d3948f3b006613095b75acd2f
179309463 <179309463@qq.com>
2016-05-30 15:11:32 +0800

    comment backup

--- src/js/controllers/bootstrap.js
+++ src/js/controllers/bootstrap.js
@@ -94,6 +94,43 @@
     };
   }])
   ; 
+  // app.controller('ModalInstanceCtrl', ['$scope', '$uibModalInstance', 'items', function($scope, $uibModalInstance, items) {
+  //   $scope.items = items;
+  //   $scope.selected = {
+  //     item: $scope.items[0]
+  //   };
+
+  //   $scope.ok = function () {
+  //     $uibModalInstance.close($scope.selected.item);
+  //   };
+
+  //   $scope.cancel = function () {
+  //     $uibModalInstance.dismiss('cancel');
+  //   };
+  // }])
+  // ; 
+  // app.controller('ModalDemoCtrl', ['$scope', '$uibModal', '$log', function($scope, $uibModal, $log) {
+  //   $scope.items = ['item1', 'item2', 'item3'];
+  //   $scope.open = function (size) {
+  //     var modalInstance = $uibModal.open({
+  //       templateUrl: 'myModalContent.html',
+  //       controller: 'ModalInstanceCtrl',
+  //       size: size,
+  //       resolve: {
+  //         items: function () {
+  //           return $scope.items;
+  //         }
+  //       }
+  //     });
+
+  //     modalInstance.result.then(function (selectedItem) {
+  //       $scope.selected = selectedItem;
+  //     }, function () {
+  //       $log.info('Modal dismissed at: ' + new Date());
+  //     });
+  //   };
+  // }])
+  // ;   
   app.controller('ModalInstanceCtrl', ['$scope', 'items', 'itemIndex', '$state', function($scope, items, itemIndex, $state) {
     $scope.items = items;
     $scope.selected = {
