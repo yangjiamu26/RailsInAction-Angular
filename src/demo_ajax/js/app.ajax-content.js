f483616a508e0b42c567e51c6e45276c8720f1e9
179309463 <179309463@qq.com>
2016-06-01 01:21:59 +0800

    ajax version of path /ajax/

--- src/demo_ajax/js/app.ajax-content.js
+++ src/demo_ajax/js/app.ajax-content.js
@@ -31,8 +31,8 @@ $(function(){
 				var path = document.location.pathname;
 
 				//for example in Ace HTML demo version we convert /ajax/index.html#page/gallery to > /ajax/content/gallery.html and load it
-				if(path.match(/(\/ajax\/)(index\.html)?/))
-					return path.replace(/(\/ajax\/)(index\.html)?/, '/ajax/content/'+hash.replace(/^page\//, '')+'.html') ;
+				if(path.match(/(ajax\/)(index\.html)?/))
+					return path.replace(/(ajax\/)(index\.html)?/, 'ajax/content/'+hash.replace(/^page\//, '')+'.html') ;
 
 				//for example in Ace PHP demo version we convert "ajax.php#page/dashboard" to "ajax.php?page=dashboard" and load it
 				return path + "?" + hash.replace(/\//, "=");
--- src/demo_ajax/js/ui-jp.js
+++ src/demo_ajax/js/ui-jp.js
@@ -2,18 +2,18 @@
 
   $(function(){
 
-      $("[ui-jq]").each(function(){
-        var self = $(this);
-        var options = eval('[' + self.attr('ui-options') + ']');
+      // $("[ui-jq]").each(function(){
+      //   var self = $(this);
+      //   var options = eval('[' + self.attr('ui-options') + ']');
 
-        if ($.isPlainObject(options[0])) {
-          options[0] = $.extend({}, options[0]);
-        }
+      //   if ($.isPlainObject(options[0])) {
+      //     options[0] = $.extend({}, options[0]);
+      //   }
 
-        uiLoad.load(jp_config[self.attr('ui-jq')]).then( function(){          
-          self[self.attr('ui-jq')].apply(self, options);
-        });
-      });
+      //   uiLoad.load(jp_config[self.attr('ui-jq')]).then( function(){          
+      //     self[self.attr('ui-jq')].apply(self, options);
+      //   });
+      // });
 
   });
 }(jQuery);
