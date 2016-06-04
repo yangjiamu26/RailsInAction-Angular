a61f5d9ce46526b5fea00cd7b2f22009a7e51044
179309463 <179309463@qq.com>
2016-06-05 00:13:04 +0800

    angular.panels

--- demo/angulr/tpl/app.js
+++ demo/angulr/tpl/app.js
@@ -84,4 +84,74 @@ angular.module('app')
         });
       }
 
+      $scope.leftOpen = function () {
+        $scope.$broadcast('leftHello', {message : $scope.message});
+      };
+
+      $scope.rightOpen = function () {
+        $scope.$broadcast('rightHello', {message : $scope.message});
+      };
+
+      $scope.topOpen = function () {
+        $scope.$broadcast('topHello', {message : $scope.message});
+      };
+
+      $scope.bottomOpen = function () {
+        $scope.$broadcast('bottomHello', {message : $scope.message});
+      };
+
+
   }]);
+
+//left panel controller
+angular.module('app').controller('PanelLeftCtrl', ['$scope', 'panels', function ($scope, panels) {
+
+  $scope.$on('leftHello', function(event, args) {
+
+    $scope.message = args.message;
+
+    panels.open("test01");
+  });
+}]);
+
+
+//right panel controller
+angular.module('app').controller('PanelRightCtrl', ['$scope', 'panels', function ($scope, panels) {
+
+  $scope.$on('rightHello', function(event, args) {
+
+    $scope.message = args.message;
+
+    panels.open("test02");
+  });
+}]);
+
+
+//top panel controller
+angular.module('app').controller('PanelTopCtrl', ['$scope', 'panels', function ($scope, panels) {
+
+  $scope.$on('topHello', function(event, args) {
+
+    $scope.message = args.message;
+
+    panels.open("test03");
+  });
+}]);
+
+
+//bottom panel controller
+angular.module('app').controller('PanelBottomCtrl', ['$scope', 'panels', function ($scope, panels) {
+
+  $scope.$on('bottomHello', function(event, args) {
+
+    $scope.message = args.message;
+
+    panels.open("test04");
+  });
+
+  //close callback
+  $scope.bottomClose = function () {
+
+    window.alert('Close Callback!!');
+  };
+}]);
