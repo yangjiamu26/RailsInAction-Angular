468a6c639bd6818ee094b09b3609dd59b4f24a4a
chenjie <chenjie@winhong.com>
2016-06-30 14:01:11 +0800

    增加webapp路径

---
+++ src/csc_html/webapp/assets/lib/angulr/js/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+window.UEDITOR_HOME_URL = "../../lib/angular-bdeditor/";
+
+angular.module('app', [
+    'ngAria',
+    'ngLocale',
+    'ngMessages',
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch',
+    'ngStorage',
+    'LocalStorageModule',
+    'ui.router',
+    'ui.router.menus',
+    'ui.router.tabs',
+    'ncy-angular-breadcrumb',
+    'boundstate.auth',
+    'ng-token-auth',
+    'permission',
+    'permission.ui',
+    'ui.bootstrap',
+    'ui.router.modal',
+    'ui.utils',
+    'ui.load',
+    'ui.jq',
+    'oc.lazyLoad',
+    'pascalprecht.translate',
+    'ngMaterial',
+    'textAngular',
+    'angular.filter',
+    'angular.panels',
+    'ngIdle',
+    'apiMock'
+]);
