0ff64c9bcbed2b9392fffef4054759e0c8515128
[pklong020] <[357248709@qq.com]>
2016-07-06 17:58:45 +0800

    cvm pad 业务系统部分

--- src/cvm_ipad/tpl/business/index.js
+++ src/cvm_ipad/tpl/business/index.js
@@ -15,8 +15,11 @@ myApp.onPageInit("business-index", function(page) {
       RestServiceJs(BASE_URL+"/busdomain").query({},function(data){
         self.busdomainNum(data.busdomainNum);
         self.projectNum(data.projectNum);
+        initPool_cpu_chart();
+        initPool_memory_chart();
+        initPool_storage_chart();
       });
-      RestServiceJs(BASE_URL+"/busdomain/projects").query({"busdomainId":CVM_PAD.dcId},function(data){
+      RestServiceJs(BASE_URL+"/busdomain/projects").query({"vdcId":''},function(data){
         self.loading = false;
         if(!is_loadMore){
           myApp.pullToRefreshDone();
@@ -46,3 +49,218 @@ myApp.onPageInit("business-index", function(page) {
     viewModel.loadData(true);
   });
 });
+
+
+// 资源池-cpu占比图
+function initPool_cpu_chart() {
+    $('#pool_cpu_chart').highcharts({
+      chart: {
+          marginTop: 0,
+          plotBackgroundColor: null,
+          plotBorderWidth: null,
+          plotShadow: false,
+          backgroundColor: "none"
+      },
+      exporting:{
+          enabled: false
+      },
+      credits:{
+          enabled: false,
+          text : ""
+      },
+
+      title: {
+          floating:true,
+          text: ''
+      },
+      legend:{
+        enabled:true,
+        margin: 0,
+        layout: 'vertical',
+        backgroundColor:"none",
+        borderColor:"none",
+        itemStyle: {
+          
+          fontWeight: 'normal',
+          fontSize:'12px'
+        },
+        labelFormat: '{name}：<b>{y}</b>GHz',
+      },
+      plotOptions: {
+          pie: {
+              innerSize: '70%',
+              borderWidth:1,
+              allowPointSelect: false,
+              cursor: 'pointer',
+              dataLabels: {
+                  enabled: true,
+                  color:'#6d6d72',
+                  distance: -25,
+                  style:{
+                    fontSize:'12px',
+                    fontWeight:'normal'
+                  },
+                  format: '{point.percentage:.1f}%'
+              },
+              showInLegend: true
+          }
+      },
+      series: [{
+          type: 'pie',
+          name: 'CPU',
+          data: [{
+                  name: '已用',
+                  y: 1.93,
+                  color:"#ffd800"
+              },
+              {
+                  name: '未用',
+                  y: 35.19,
+                  color:"#59cb5c"
+              }
+          ]
+      }]
+    });   
+}
+// 资源池-内存占比图
+function initPool_memory_chart() {
+    $('#pool_memory_chart').highcharts({
+      chart: {
+          marginTop: 0,
+          plotBackgroundColor: null,
+          plotBorderWidth: null,
+          plotShadow: false,
+          backgroundColor: "none"
+      },
+      exporting:{
+          enabled: false
+      },
+      credits:{
+          enabled: false,
+          text : ""
+      },
+
+      title: {
+          floating:true,
+          text: ''
+      },
+      legend:{
+        enabled:true,
+        margin: 0,
+        layout: 'vertical',
+        backgroundColor:"none",
+        borderColor:"none",
+        itemStyle: {
+          
+          fontWeight: 'normal',
+          fontSize:'12px'
+        },
+        labelFormat: '{name}：<b>{y}</b>GHz',
+      },
+      plotOptions: {
+          pie: {
+              innerSize: '70%',
+              borderWidth:1,
+              allowPointSelect: false,
+              cursor: 'pointer',
+              dataLabels: {
+                  enabled: true,
+                  color:'#6d6d72',
+                  distance: -25,
+                  style:{
+                    fontSize:'12px',
+                    fontWeight:'normal'
+                  },
+                  format: '{point.percentage:.1f}%'
+              },
+              showInLegend: true
+          }
+      },
+      series: [{
+          type: 'pie',
+          name: '内存',
+          data: [{
+                  name: '已用',
+                  y: 112.64,
+                  color:"#ffd800"
+              },
+              {
+                  name: '未用',
+                  y: 76.11,
+                  color:"#59cb5c"
+              }
+          ]
+      }]
+    });   
+}
+// 资源池-存储占比图
+function initPool_storage_chart() {
+    $('#pool_storage_chart').highcharts({
+      chart: {
+          marginTop: 0,
+          plotBackgroundColor: null,
+          plotBorderWidth: null,
+          plotShadow: false,
+          backgroundColor: "none"
+      },
+      exporting:{
+          enabled: false
+      },
+      credits:{
+          enabled: false,
+          text : ""
+      },
+
+      title: {
+          floating:true,
+          text: ''
+      },
+      legend:{
+        enabled:true,
+        margin: 0,
+        layout: 'vertical',
+        backgroundColor:"none",
+        borderColor:"none",
+        itemStyle: {
+          
+          fontWeight: 'normal',
+          fontSize:'12px'
+        },
+        labelFormat: '{name}：<b>{y}</b>GHz',
+      },
+      plotOptions: {
+          pie: {
+              innerSize: '70%',
+              borderWidth:1,
+              allowPointSelect: false,
+              cursor: 'pointer',
+              dataLabels: {
+                  enabled: true,
+                  color:'#6d6d72',
+                  distance: -25,
+                  style:{
+                    fontSize:'12px',
+                    fontWeight:'normal'
+                  },
+                  format: '{point.percentage:.1f}%'
+              },
+              showInLegend: true
+          }
+      },
+      series: [{
+          type: 'pie',
+          name: '存储',
+          data: [{
+                  name: '已用',
+                  y: 4.73,
+                  color:"#ffd800"
+              },
+              {
+                  name: '未用',
+                  y: 3.81,
+                  color:"#59cb5c"
+              }
+          ]
+      }]
+    });   
+}
