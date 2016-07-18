3e9044d4f5702e95ffd117209b58b9db35958b50
32642743@qq.com <32642743@qq.com>
2016-07-18 11:28:16 +0800

    alert

--- src/csc_html/WebRoot/assets/js/app.js
+++ src/csc_html/WebRoot/assets/js/app.js
@@ -39,8 +39,31 @@ if ($(".modal").length > 0) {
 
 
 function systemMessage(){
-	this.alert = function(msg,callBack){
-		$("body").append('<div class="modal fade" id="systemMessageModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">'+
+	this.alert = function(msg){
+		$("body").append('<div class="modal fade" id="systemMessageAlertModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">'+
+  							'<div class="modal-dialog modal-sm" role="document">' +
+							    '<div class="modal-content">' +
+							      '<div class="modal-header">' +
+							        '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
+							        '<h4 class="modal-title" id="myModalLabel">系统信息</h4>' +
+							      '</div>' +
+							      '<div class="modal-body">' +
+							        msg +
+							      '</div>' +
+							      '<div class="modal-footer">' +
+							        '<button type="button" class="btn btn-primary" data-dismiss="modal">确定</button>' +
+							      '</div>' +
+							    '</div>' +
+							'</div>' +
+						'</div>');
+		var modal = $("#systemMessageAlertModal");
+		modal.modal('show');
+		modal.on("hidden.bs.modal",function(){
+			modal.remove();
+		})
+	};
+	this.confirm = function(msg,callBack){
+		$("body").append('<div class="modal fade" id="systemMessageConfirmModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">'+
   							'<div class="modal-dialog modal-sm" role="document">' +
 							    '<div class="modal-content">' +
 							      '<div class="modal-header">' +
@@ -57,16 +80,18 @@ function systemMessage(){
 							    '</div>' +
 							'</div>' +
 						'</div>');
-		var modal = $("#systemMessageModal");
+		var modal = $("#systemMessageConfirmModal");
 		modal.modal('show');
 		modal.on("hidden.bs.modal",function(){
 			modal.remove();
 		})
 		$("#systemMessageModalOk",modal).on("click",function(){
-			if(callBack)
-				callBack();
+			if(callBack){
+				setTimeout(function(){
+					callBack();
+				},1000);
+			}	
 		});
-		
 	};
 };
 var systemMsg = new systemMessage();
