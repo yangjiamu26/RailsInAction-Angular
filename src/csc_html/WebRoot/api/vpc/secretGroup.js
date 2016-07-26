21a890301cf2cb4a908b7a6a500a94936fd6ec53
chenjie <chenjie@winhong.com>
2016-07-26 16:37:56 +0800

    更新开发人员代码+“我的工作”模块，未提交的服务申请，分成批量和单个的不同

--- src/csc_html/WebRoot/api/vpc/secretGroup.js
+++ src/csc_html/WebRoot/api/vpc/secretGroup.js
@@ -32,11 +32,38 @@
 			 * 获取安全组列表
 			 * @param param  查询条件
 			 */
-			getSecretGroups:function(param,callback,errorCallback){
+			getSecretGroups:function(param,callback){
 				 var param = csc.util.httpQueryParamConvert(param);
 				 csc.rest.get('api/v5.0.0/secretGroups?'+param,function(data){
 					     callback(data);
 	    		    }); 
 			},
+			/**
+			 *查询安全组规则
+			 */
+			getSecretGroupRules:function(sgUuid,param,callback){
+				 var param = csc.util.httpQueryParamConvert(param);
+				 csc.rest.get('api/v5.0.0/secretGroups/'+sgUuid+'/secretGroupRules?'+param,function(data){
+					     callback(data);
+	    		    }); 
+			},
+			/**
+			 * 删除安全组规则
+			 *  @param uuid 
+			 */
+			delSecretGroupRules:function(sgUuid,uuid,callback){
+				csc.rest.del('api/v5.0.0/secretGroups/'+sgUuid+'/secretGroupRules/'+uuid,function(data){
+					 callback(data)
+   		        })
+			},
+			/**
+			 * 添加安全组规则
+			 * @param 
+			 */
+			addSecretGroupRule:function(sgUuid,param,callback,errorCallback){
+				 csc.rest.post('api/v5.0.0/secretGroups/'+sgUuid+'/secretGroupRules',param,function(data){
+					     callback(data);
+	    		    }); 
+			}
 	};
 }(window.jQuery);
