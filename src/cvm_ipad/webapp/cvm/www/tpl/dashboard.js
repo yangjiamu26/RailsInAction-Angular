d684ffca48955429247e0fc2b6ed2914fbc6eee6
[pklong020] <[357248709@qq.com]>
2016-09-05 10:56:42 +0800

    总览加退出、去筛选背景底色、修复详情touch问题

--- src/cvm_ipad/webapp/cvm/www/tpl/dashboard.js
+++ src/cvm_ipad/webapp/cvm/www/tpl/dashboard.js
@@ -1,4 +1,18 @@
 myApp.onPageInit("dashboard", function(page) {
+  $$('#logout').on('click', function (){
+    myApp.confirm('确定退出当前用户吗？',function(){
+      myApp.Login_Again = true;
+      myApp.showAssisTime = false;
+      myApp.addView('#view-login', {dynamicNavbar: false,domCache: true}).router.load({url: 'tpl/login.html',animatePages: false});
+      Storage.removeItem("userInfo");
+      USER_INFO.password = '';
+      USER_INFO.token = '';
+      USER_INFO.tokenKey = '';
+      Storage.setItem("userInfo",JSON.stringify(USER_INFO));
+      myApp.closeModal('.popup.modal-in');
+      myApp.loginScreen();
+    });
+  });
   // 首页cpu占比图
 var initTotal_cpu_chart = function(data) {
     $(page.container).find('#total_cpu_chart').highcharts({
